3560058902ea355e905d1a6373ac670ee0edda7c
alinr10 <98055702+alinr10@users.noreply.github.com>
2024-06-04 18:05:15 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use(upload.single('profilePhoto'));
 
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://sell-wisely.netlify.app");
   res.header("Access-Control-Allow-Methods", "GET, POST ,PUT,DELETE,PATCH");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, x-access-token");
   res.header("Access-Control-Allow-Credentials", "true");
